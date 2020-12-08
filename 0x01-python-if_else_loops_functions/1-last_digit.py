13d58d5ac2b15a29748fd7089c2c08feebab492c
Jeff Martinez <2408@holbertonschool.com>
2020-12-08 20:25:31 +0000

    Adding a script to identify the last digit of a random number.

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    negative = number * -1
+    last = negative % 10
+
+else:
+    last = number % 10
+
+if last == 0:
+    print("Last digit of 0 is 0 and is 0")
+
+elif last > 5:
+    print("Last digit of", number, "is", last, "and is greater than 5")
+
+else:
+    print("Last digit of", number, "is", last, "and is less than 6 and not 0")
