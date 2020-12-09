472ff5b288df7b4c26dcba3bd47b72194cd78905
Jeff Martinez <2408@holbertonschool.com>
2020-12-09 00:29:41 +0000

    Changed the elif for if statement.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,8 +12,8 @@ else:
 if last == 0:
     print("Last digit of 0 is 0 and is 0")
 
-elif last > 5:
+if last > 5:
     print("Last digit of", number, "is", last, "and is greater than 5")
 
-else:
+if last < 6 and last != 0:
     print("Last digit of", number, "is", last, "and is less than 6 and not 0")
