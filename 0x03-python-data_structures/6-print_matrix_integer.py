2bc701cd74c14c341d9bd116bf72039ed691e1d5
Jeff Martinez <2408@holbertonschool.com>
2020-12-16 01:45:17 +0000

    Added if to check on parameter.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,14 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    cols = len(matrix[0])
-    for i in range(cols):
-        for j in range(rows):
-            if j == rows - 1:
-                print("{:d}".format(matrix[i][j]), end="")
+    if matrix:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for i in range(cols):
+            for j in range(rows):
+                if j == rows - 1:
+                    print("{:d}".format(matrix[i][j]), end="")
+                else:
+                    print("{:d} ".format(matrix[i][j]), end="")
             else:
-                print("{:d} ".format(matrix[i][j]), end="")
-        else:
-            print()
+                print()
