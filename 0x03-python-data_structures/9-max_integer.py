7048cf07e800f1f7ff939df9dbc8dfd5cce08a9a
Jeff Martinez <2408@holbertonschool.com>
2020-12-16 01:48:42 +0000

    Added if to check on parameter.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,9 @@
 
 
 def max_integer(my_list=[]):
-    maxx = my_list[0]
-    if len(my_list) == 0:
-        return (None)
-    for i in my_list:
-        if i > maxx:
-            maxx = i
-    return (maxx)
+    if my_list:
+        maxx = my_list[0]
+        for i in my_list:
+            if i > maxx:
+                maxx = i
+        return (maxx)
