f6fa077fb2cd7e78f90294f1dd54f644ced9fa0e
Jeff Martinez <2408@holbertonschool.com>
2020-12-17 21:33:43 +0000

    Adding an if to check on parameters.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 
 
 def roman_to_int(roman_string):
+    if type(roman_string) != str or len(roman_string) == 0:
+        return None
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
              'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     num = 0
