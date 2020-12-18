b313e3fa213796d4bc66b805258cef371a7578f9
Jeff Martinez <2408@holbertonschool.com>
2020-12-18 16:07:49 +0000

    Changed the if.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,10 @@
 
 
 def uniq_add(my_list=[]):
-    if my_list:
+    if len(my_list) != 0:
         result = 0
         for x in set(my_list):
             result += x
         return result
+    else:
+        return None
