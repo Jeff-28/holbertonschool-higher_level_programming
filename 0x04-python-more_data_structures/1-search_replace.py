7d218007829016728b2ccf5cf99318282a601ed8
Jeff Martinez <2408@holbertonschool.com>
2020-12-18 15:42:37 +0000

    Changed code approach.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,4 @@
 
 
 def search_replace(my_list, search, replace):
-    if my_list:
-        new = []
-        for value in my_list:
-            new.append(value if value != search else replace)
-        return new
+    return list(map(lambda x: x if x != search else replace, my_list))
