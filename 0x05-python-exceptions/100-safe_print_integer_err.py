dd27c1e7ef8cb4e397398c199ed0c3af61433017
Jeff Martinez <2408@holbertonschool.com>
2021-01-04 20:18:51 +0000

    Fixed code for stderr.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,10 @@
 
 
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
