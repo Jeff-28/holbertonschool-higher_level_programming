390afbc83e1d63977471b9e63352ef5214aea172
Jeff Martinez <2408@holbertonschool.com>
2021-01-05 20:43:36 +0000

    Adding task 3 file.

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Square Module - creates and defines the square class.
+"""
+
+
+class Square:
+    """A new class created for an object oriented programming
+        project. It takes the size argument."""
+
+    def __init__(self, size=0):
+        """Defines the private attribute size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
