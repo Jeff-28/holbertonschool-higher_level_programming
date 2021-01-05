403656c9458d722760722fa74ab32be97c8fefd0
Jeff Martinez <2408@holbertonschool.com>
2021-01-05 20:34:27 +0000

    Added documentation.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+Square Module - creates and defines the square class.
+"""
 
 
 class Square:
+    """A new class created for an object oriented programming
+        project. It takes the size argument."""
+
     def __init__(self, size=0):
+        """Defines the private attribute size."""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
