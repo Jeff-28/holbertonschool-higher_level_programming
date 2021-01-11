876e72dc66a39323926a2e0d98399a756d2f1d24
Jeff Martinez <2408@holbertonschool.com>
2021-01-11 17:06:10 +0000

    Adding files.

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+"""
+Rectangle Module - creates and defines rectangles.
+"""
+
+class Rectangle:
+    """A class to work with Rectangle objects."""
+    def __init__(self, width=0, height=0):
+        """Creates a new object."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the attribute width."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Defines the width with the value."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the attribute height."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Defines the height with the value."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __repr__(self):
+        """Returns the object representation in string format."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __str__(self):
+        """Returns a string representation of the object."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rec_str = (("#" * self.__width) + "\n") * self.__height
+        return rec_str[:-1]
