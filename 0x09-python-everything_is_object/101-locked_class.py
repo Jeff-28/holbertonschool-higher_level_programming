c5b4e7ee20fe664d1ab411d667eab2619c32e1c0
Jeff Martinez <2408@holbertonschool.com>
2021-01-13 18:23:56 +0000

    Pep8 checked.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -13,5 +13,5 @@ class LockedClass:
         if attribute == "first_name":
             self.__dict__[attribute] = value
         else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
-
+            raise AttributeError("'LockedClass' object has no attribute '" +
+                                 attribute + "'")
