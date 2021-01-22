e04b0083eb8f2c13bdbe222d49d677382fb18c38
Jeff Martinez <2408@holbertonschool.com>
2021-01-22 02:18:59 +0000

    Fixed code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,7 +6,8 @@ I/O Module
 import sys
 
 
-line_dict = {}
+line_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 count = 0
 total_size = 0
 try:
@@ -18,9 +19,6 @@ try:
             if status in line_dict:
                 line_dict[status] += 1
                 count += 1
-            else:
-                line_dict[status] = 1
-                count += 1
             try:
                 total_size += int(strings[-1])
                 if x == count:
@@ -30,14 +28,16 @@ try:
                     continue
         if count % 10 == 0:
             print("File size: {}".format(total_size))
-            ordered = sorted(line_dict.keys())
-            for key in ordered:
-                print("{}: {}".format(key, line_dict[key]))
+            for key, value in sorted(line_dict.items()):
+                if value:
+                    print("{}: {}".format(key, value))
     print("File size: {}".format(total_size))
-    for key in ordered:
-        print("{}: {}".format(key, line_dict[key]))
+    for key, value in sorted(line_dict.items()):
+        if value:
+            print("{}: {}".format(key, value))
 
 except KeyboardInterrupt:
     print("File size: {}".format(total_size))
-    for key in ordered:
-        print("{}: {}".format(key, line_dict[key]))
+    for key, value in sorted(line_dict.items()):
+        if value:
+            print("{}: {}".format(key, value))
