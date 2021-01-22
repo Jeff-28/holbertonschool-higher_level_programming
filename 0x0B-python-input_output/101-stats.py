27075fd273d235efeb66e69c53d17ecec5aa4393
Jeff Martinez <2408@holbertonschool.com>
2021-01-22 01:40:48 +0000

    Fixed code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,25 +4,29 @@ I/O Module
 """
 
 import sys
-import io
 
 
 line_dict = {}
 count = 0
 total_size = 0
-for line in sys.stdin:
-    strings = line.split()
-    status = strings[-2]
-    total_size += int(strings[-1])
-    count += 1
-    if status not in line_dict.keys():
-        line_dict[status] = 1
-    else:
-        line_dict[status] += 1
-    if count == 10:
-        print("File size: {}".format(total_size))
-        ordered = sorted(line_dict.keys())
-        for key in ordered:
-            print("{}: {}".format(key, line_dict[key]))
-        count = 0
-        continue
+try:
+    for line in sys.stdin:
+        strings = line.split()
+        status = strings[-2]
+        total_size += int(strings[-1])
+        count += 1
+        if status not in line_dict.keys():
+            line_dict[status] = 1
+        else:
+            line_dict[status] += 1
+        if count == 10:
+            print("File size: {}".format(total_size))
+            ordered = sorted(line_dict.keys())
+            for key in ordered:
+                print("{}: {}".format(key, line_dict[key]))
+            count = 0
+
+except KeyboardInterrupt:
+    print("File size: {}".format(total_size))
+    for key in ordered:
+        print("{}: {}".format(key, line_dict[key]))
