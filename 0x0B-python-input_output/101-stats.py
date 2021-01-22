d87ad74d6d3fbc6fc98cab313fbc7b643c7027e6
Jeff Martinez <2408@holbertonschool.com>
2021-01-22 01:18:24 +0000

    Adding file

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+I/O Module
+"""
+
+import sys
+import io
+
+
+line_dict = {}
+count = 0
+total_size = 0
+for line in sys.stdin:
+    strings = line.split()
+    status = strings[-2]
+    total_size += int(strings[-1])
+    count += 1
+    if status not in line_dict.keys():
+        line_dict[status] = 1
+    else:
+        line_dict[status] += 1
+    if count == 10:
+        print("File size: {}".format(total_size))
+        ordered = sorted(line_dict.keys())
+        for key in ordered:
+            print("{}: {}".format(key, line_dict[key]))
+        count = 0
+        continue
