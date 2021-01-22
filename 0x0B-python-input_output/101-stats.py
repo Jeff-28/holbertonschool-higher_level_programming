3e69b5b31270dab126ace57e1afd9c439052b3d1
Jeff Martinez <2408@holbertonschool.com>
2021-01-22 02:08:17 +0000

    Fixed code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,18 +13,28 @@ try:
     for line in sys.stdin:
         strings = line.split()
         status = strings[-2]
-        total_size += int(strings[-1])
-        count += 1
-        if status not in line_dict.keys():
-            line_dict[status] = 1
-        else:
+        x = count
+        if status in line_dict:
             line_dict[status] += 1
-        if count == 10:
+            count += 1
+        else:
+            line_dict[status] = 1
+            count += 1
+        try:
+            total_size += int(strings[-1])
+            if x == count:
+                count += 1
+        except:
+            if x == count:
+                continue
+        if count % 10 == 0:
             print("File size: {}".format(total_size))
             ordered = sorted(line_dict.keys())
             for key in ordered:
                 print("{}: {}".format(key, line_dict[key]))
-            count = 0
+    print("File size: {}".format(total_size))
+    for key in ordered:
+        print("{}: {}".format(key, line_dict[key]))
 
 except KeyboardInterrupt:
     print("File size: {}".format(total_size))
