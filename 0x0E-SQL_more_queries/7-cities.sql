608f6ad28326bc31254eb99b18b2200c8c36da53
Jeff Martinez <2408@holbertonschool.com>
2021-03-02 20:02:01 +0000

    fixed foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,6 +9,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY_KEY,
-    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
+    state_id INT NOT NULL FOREIGN KEY(states_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
