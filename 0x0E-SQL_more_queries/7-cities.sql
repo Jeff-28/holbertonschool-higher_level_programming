78ba9a07d5abe0186f57c585efed4135d59a5897
Jeff Martinez <2408@holbertonschool.com>
2021-03-02 20:04:11 +0000

    fixed primary key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,7 +8,9 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY_KEY,
-    state_id INT NOT NULL FOREIGN KEY(states_id) REFERENCES states(id),
+    id INT UNIQUE AUTO_INCREMENT NOT NULL
+    PRIMARY KEY(id),
+    state_id INT NOT NULL
+    FOREIGN KEY(states_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
