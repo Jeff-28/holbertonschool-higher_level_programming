969052790be9101b0beb26e37afd67f4a7b0af52
Jeff Martinez <2408@holbertonschool.com>
2021-04-03 00:30:21 +0000

    new files

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that creates the State “California” with
-the City “San Francisco” from the database hbtn_0e_100_usa
+A script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
 """
 
 from sys import argv
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = State(name="California")
-    state.cities = [City(name="San Francisco")]
-    session.add(state)
-    session.commit()
-    session.close()
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-A script that creates the State “California” with
-the City “San Francisco” from the database hbtn_0e_100_usa
+A script that lists all City objects from the database hbtn_0e_101_usa
 """
 
 from sys import argv
@@ -17,8 +16,6 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = State(name="California")
-    state.cities = [City(name="San Francisco")]
-    session.add(state)
-    session.commit()
-    session.close()
+    cities = session.query(City).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
