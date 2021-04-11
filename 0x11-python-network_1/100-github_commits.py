47a686d05b169fb6fd2a8076a144d121ebd7d71d
Jeff Martinez <2408@holbertonschool.com>
2021-04-11 19:59:39 +0000

    new file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Please list 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+    https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+A Python script that takes 2 arguments in order to solve this challenge:
+    The first argument will be the repository name
+    The second argument will be the owner name
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get('https://api.github.com/repos/{}/{}/\
+                     commits'.format(argv[2], argv[1]))
+    data = r.json()
+    if data:
+        for j in range(10):
+            print('{}: {}'.format(data[j].get('sha'),
+                                  data[j].get('commit')
+                                  .get('author').get('name')))
