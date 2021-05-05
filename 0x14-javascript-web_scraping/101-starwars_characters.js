441821cc6c772cba04d9c347a4b7beca4a3e40a9
Jeff Martinez <2408@holbertonschool.com>
2021-05-05 00:30:25 +0000

    changed the initial request

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,25 @@
 
 const args = process.argv.slice(2);
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + args[0], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/', function (error, response, body) {
   if (error) {
     console.error(error);
   }
   const dict = JSON.parse(body);
-  const characters = dict.characters;
-  for (const url of characters) {
-    request(url, function (err, response, body) {
-      if (err) {
-        console.error(err);
+  const films = dict.results;
+  const id = 'https://swapi-api.hbtn.io/api/films/' + args[0] + '/';
+  for (const film of films) {
+    if (film.url === id) {
+      for (const url of film.characters) {
+        request(url, function (err, response, body) {
+          if (err) {
+            console.error(err);
+          }
+          const character = JSON.parse(body);
+          console.log(character.name);
+        });
       }
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
+      break;
+    }
   }
 });
